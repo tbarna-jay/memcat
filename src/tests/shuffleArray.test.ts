e2eb775eef9df18a373be31738190620623bce6a
Barna Tamas <barnatamas@gmail.com>
2023-12-08 22:01:41 +0100

    feat: add shuffle unit test

---
+++ src/tests/shuffleArray.test.ts
@@ -0,0 +1,38 @@
+import shuffleArray from "../helpers/shuffleArray";
+
+// Test the shuffleArray function
+describe("shuffleArray", () => {
+  it("should shuffle an array", () => {
+    // Arrange
+    const originalArray = [1, 2, 3, 4, 5];
+
+    // Act
+    const shuffledArray = shuffleArray([...originalArray]);
+
+    // Assert
+    expect(shuffledArray).not.toEqual(originalArray); // Check that the array is shuffled
+    expect(shuffledArray.sort()).toEqual(originalArray.sort()); // Check that all elements are still present
+  });
+
+  it("should handle an empty array", () => {
+    // Arrange
+    const originalArray: number[] = [];
+
+    // Act
+    const shuffledArray = shuffleArray([...originalArray]);
+
+    // Assert
+    expect(shuffledArray).toEqual(originalArray); // Shuffling an empty array should result in an empty array
+  });
+
+  it("should handle an array with a single element", () => {
+    // Arrange
+    const originalArray = [42];
+
+    // Act
+    const shuffledArray = shuffleArray([...originalArray]);
+
+    // Assert
+    expect(shuffledArray).toEqual(originalArray); // Shuffling an array with a single element should result in the same array
+  });
+});
